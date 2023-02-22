36205838fa6f8b015cdeb85cd265d10627398cd6
37529 <VadzimKuzmenka@mail.ru>
2023-02-22 15:33:20 +0400

    commit to dev

--- src/main/java/com/example/hotelsimpleservice/security/SecurityService.java
+++ src/main/java/com/example/hotelsimpleservice/security/SecurityService.java
@@ -9,4 +9,5 @@ import org.springframework.stereotype.Service;
 @Service
 public interface SecurityService extends UserDetailsService {
      UserDetails loadUserByUsername(String login) throws UsernameNotFoundException;
+
 }
